25b904fcb6750f7d9fe0ab5b2745995f829ea5d2
Philip Giuliani <philip.giuliani@keepinmind.info>
2022-10-20 16:12:02 +0000

    Add framequeue probe

---
+++ lib/membrane/video_mixer/frame_queue/probe.ex
@@ -0,0 +1,22 @@
+defmodule VideoMixer.FrameQueue.Probe do
+  use Probe
+  alias VegaLite, as: Vl
+
+  @impl true
+  def event_name(), do: [:video_mixer, :frame_queue]
+
+  @impl true
+  def compile(data, opts) do
+    field = Keyword.get(opts, :field, "count")
+
+    Vl.new(title: "Number of items the queue over time", height: 1080, width: 1920)
+    |> Vl.data_from_values(data)
+    |> Vl.mark(:line)
+    |> Vl.encode_field(:x, "t", type: :ordinal, time_unit: "hoursminutesseconds")
+    |> Vl.encode_field(:color, "label", type: :nominal)
+    |> Vl.encode_field(:y, field,
+      type: :quantitative,
+      scale: [zero: false]
+    )
+  end
+end
